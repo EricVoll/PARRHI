2cdabecbd0be60c90bd409698817bdebaaad1074
Eric Vollenweider <eric.vollenweider@gmail.com>
2019-04-01 15:18:48 +0200

    Trigger classes

---
+++ 04_PARRHI_Library/PARRHI/Objects/Triggers/DistanceTrigger.cs
@@ -0,0 +1,39 @@
+﻿using PARRHI.Objects.Points;
+using PARRHI.Objects.TriggerActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARRHI.Objects.Triggers
+{
+    public class DistanceTrigger : Trigger
+    {
+        const double Tolerance = 10;
+        public DistanceTrigger(string id, Point point1, Point point2, double distance, TriggerAction triggerAction1, TriggerAction triggerAction2) : this(id, point1, point2, distance, triggerAction1)
+        {
+            TriggerAction2 = triggerAction2;
+        }
+        public DistanceTrigger(string id, Point point1, Point point2, double distance, TriggerAction triggerAction1) : this(id, point1, point2, distance)
+        {
+            TriggerAction1 = triggerAction1;
+        }
+        public DistanceTrigger(string id, Point point1, Point point2, double distance) : base(id)
+        {
+            CheckTrigger = CheckTriggerFunc;
+            P1 = point1;
+            P2 = point2;
+            Distance = distance;
+        }
+
+        Point P1 { get; set; }
+        Point P2 { get; set; }
+        double Distance { get; set; }
+
+        private bool CheckTriggerFunc(object param)
+        {
+            return (P2 - P1).Magnitude < Tolerance;
+        }
+    }
+}
---
+++ 04_PARRHI_Library/PARRHI/Objects/Triggers/TimeTrigger.cs
@@ -0,0 +1,46 @@
+﻿using PARRHI.Objects.State;
+using PARRHI.Objects.TriggerActions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARRHI.Objects.Triggers
+{
+    public class TimeTrigger : Trigger
+    {
+        public TimeTrigger(string id, bool canTrigger, long triggertime, TriggerAction triggerAction1, TriggerAction triggerAction2) : this(id, canTrigger, triggertime, triggerAction1)
+        {
+            TriggerAction2 = triggerAction2;
+        }
+        public TimeTrigger(string id, bool canTrigger, long triggertime, TriggerAction triggerAction1) : this(id, canTrigger, triggertime)
+        {
+            TriggerAction1 = triggerAction1;
+        }
+        public TimeTrigger(string id, bool canTrigger, long triggertime) : base(id, canTrigger)
+        {
+            CanTriggerSet = SetResetTime;
+            TriggerTime = triggertime;
+            CheckTrigger = CheckTriggerFunc;
+        }
+
+        /// <summary>
+        /// Triggertime in ms after activation
+        /// </summary>
+        public long TriggerTime { get; set; } = 0;
+
+        private long TriggerTimeSinceReset { get; set; } = 0;
+
+
+        public void SetResetTime()
+        {
+            TriggerTimeSinceReset = World.Time;
+        }
+
+        private bool CheckTriggerFunc(object currentTime)
+        {
+            return ((long)currentTime - TriggerTimeSinceReset) >= TriggerTime;
+        }
+    }
+}
